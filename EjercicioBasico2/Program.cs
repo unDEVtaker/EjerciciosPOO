9b25f27b129db506ff92cd7d99a30096b159ca66
Jona Silva <elundevtaker@gmail.com>
2023-06-28 01:31:18 -0300

    resuelto ejericio 1 y 2, el ejercicio2 me da un error que hay que verificar, aumque entiendo que esta bien.

---
+++ EjercicioBasico2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Ejercicio 2: Crear una clase "Círculo" que tenga una propiedad para el radio. Incluye métodos para calcular el área y la circunferencia del círculo.
+
+Circulo circulo1 = new Circulo();
+circulo1.Radio = 2;
+
+double area = circulo1.CalcularArea;
+double circunferencia = circulo1.CalcularCircunferencia;
+
+Console.WriteLine("Area:" + area);
+Console.WriteLine("Circunferencia:" + circunferencia);
+
+
+
+
+
+
+class Circulo{
+    public double Radio { get; set; }
+
+    public double CalcularArea(){
+        return Radio*3;
+    }
+    public double CalcularCircunferencia(){
+        return 2*3*Radio;
+    }
+}

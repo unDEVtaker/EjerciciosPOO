78f5863788276a415cc3a9405c1fba0c60df0010
Jona Silva <elundevtaker@gmail.com>
2023-06-28 01:13:44 -0300

    initial commit.

---
+++ EjercicioBasico1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Ejercicio 1: Crear una clase "Rectángulo" que tenga propiedades para la longitud y el ancho. Incluye métodos para calcular el área y el perímetro del rectángulo.
+
+Rectangulo rectangulo1= new Rectangulo();
+rectangulo1.Ancho = 5;
+rectangulo1.Longitud = 3;
+
+double area = rectangulo1.CalcularArea();
+double perimetro= rectangulo1.CalcularPerimetro();
+
+Console.WriteLine("Area: " + area);
+Console.WriteLine("Perimetro: " + perimetro);
+
+class Rectangulo{
+    public double Longitud { get; set; }
+    public double Ancho { get; set; }
+
+    public double CalcularArea(){
+        return Longitud* Ancho;
+    }
+
+    public double CalcularPerimetro(){
+        return 2* (Longitud+ Ancho);
+    }
+}
+

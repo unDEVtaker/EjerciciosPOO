bff7be0dbbb21f926ac3fa90c87b427a98acd247
Jona Silva <elundevtaker@gmail.com>
2023-06-28 02:26:26 -0300

    planteo de ejercicio6.

---
+++ EjercicioBasico6/Program.cs
@@ -0,0 +1,8 @@
+﻿//Ejercicio 6: Crear una clase "CuentaBancaria" que tenga propiedades para el titular de la cuenta y el saldo. Incluye métodos para realizar depósitos, retiros y consultar el saldo actual.
+
+
+class CuentaBancaria{
+    public string Titular { get; set; }
+    public string Saldo { get; set; }
+
+}

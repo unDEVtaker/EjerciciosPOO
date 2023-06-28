a98a20b67b183f502528ef2b4113fb787c87c5cb
Jona Silva <elundevtaker@gmail.com>
2023-06-28 01:38:39 -0300

    resuelto ejericio 3.

---
+++ EjercicioBasico3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Ejercicio 3: Crear una clase "Estudiante" que tenga propiedades para el nombre y la edad. Incluye un método para imprimir los detalles del estudiante.
+
+Alumno alumno1= new Alumno();
+alumno1.Nombre="Jona";
+alumno1.Edad=22;
+
+alumno1.ImprimirDetalle();
+
+
+class Alumno {
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+
+public void ImprimirDetalle(){
+    Console.WriteLine("Nombre:" + Nombre);
+    Console.WriteLine("Edad:" + Edad);
+}
+
+
+}
